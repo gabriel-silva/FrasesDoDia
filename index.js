936b945701b8b7473a92d5829a2de1a323068ee9
Gabriel <gabriel-silva_2011@hotmail.com>
2018-02-13 14:44:37 -0300

    alterando index.js

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { Text, View, Image, AppRegistry } from 'react-native';
+
+//formatações
+const Estilos = {
+    principal: {
+        backgroundColor: '#538530',
+        flex: 1
+    }
+};
+
+const botaoPressionado = () => {
+
+}
+
+//criar o componente
+const App = () => {
+    const { principal } = Estilos;
+    return (
+        <View style={principal}>
+            <Image source={ require() } />
+        </View>
+    );
+};
 
 AppRegistry.registerComponent('FrasesDoDia', () => App);
