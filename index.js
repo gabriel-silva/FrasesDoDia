e7638b4494b34ef5474fb2c9eb1590aca7b700a5
Gabriel <gabriel-silva_2011@hotmail.com>
2018-02-13 14:59:55 -0300

    inserindo TouchableOpacity

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import { Text, View, Image, AppRegistry } from 'react-native';
+import { Text, View, Image, AppRegistry, TouchableOpacity } from 'react-native';
 
 //formatações
 const Estilos = {
     principal: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    botao: {
         backgroundColor: '#538530',
-        flex: 1
+        paddingVertical: 10,
+        paddingHorizontal: 40,
+        marginTop: 20
+    },
+    textoBotao: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold'
     }
 };
 
-const botaoPressionado = () => {
+const botaoPressionado = () =>{
 
 }
 
 //criar o componente
 const App = () => {
-    const { principal } = Estilos;
+    const { principal, botao, textoBotao } = Estilos;
     return (
-        <View style={principal}>
-            <Image source={ require() } />
+        <View style={ principal }>
+            
+            <Image source={ require('./imgs/uvas.png') } />
+            
+            <TouchableOpacity style={ botao }>
+                <Text style={ textoBotao }>Nova Frase</Text>
+            </TouchableOpacity>
+        
         </View>
     );
 };
