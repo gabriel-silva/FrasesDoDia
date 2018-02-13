c49f48c291eaad21205a15bbeb17f40629121d74
Gabriel <gabriel-silva_2011@hotmail.com>
2018-02-13 15:22:51 -0300

    terminando frases do dia

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Text, View, Image, AppRegistry, TouchableOpacity } from 'react-native';
+import {
+    Text,
+    View,
+    Image,
+    AppRegistry,
+    TouchableOpacity,
+    Alert
+} from 'react-native';
 
 //formatações
 const Estilos = {
@@ -21,22 +28,37 @@ const Estilos = {
     }
 };
 
-const botaoPressionado = () =>{
+const gerarNovaFrase = () => {
+    var numeroAletorio = Math.random();
+    numeroAletorio = Math.floor(numeroAletorio * 5);
 
-}
+    var frases = Array();
+    frases[0] = 'aa';
+    frases[1] = 'bb';
+    frases[2] = 'cc';
+    frases[3] = 'dd';
+    frases[4] = 'ee';
+
+    var fraseEscolhida = frases[numeroAletorio];
+
+    Alert.alert(fraseEscolhida);
+};
 
 //criar o componente
 const App = () => {
     const { principal, botao, textoBotao } = Estilos;
     return (
-        <View style={ principal }>
-            
-            <Image source={ require('./imgs/uvas.png') } />
-            
-            <TouchableOpacity style={ botao }>
-                <Text style={ textoBotao }>Nova Frase</Text>
+        <View style={principal}>
+
+            <Image source={require('./imgs/uvas.png')} />
+
+            <TouchableOpacity
+                onPress={gerarNovaFrase}
+                style={botao}
+            >
+                <Text style={textoBotao}>Nova Frase</Text>
             </TouchableOpacity>
-        
+
         </View>
     );
 };
